bdd9984f1c6d8f688efb2e8651c2b90fca903c51
kevinhsu <kevinhsu@synology.com>
2017-04-29 21:16:15 +0800

    Try the library

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ var g = G$('John', 'Doe');
 
 g.greet().setLang('es').greet(true);
 
-console.log(g);
+$('#login').click(function() {
+	var loginGrtr = G$('John', 'Doe');
+
+	$('#logindiv').hide();
+
+	loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
